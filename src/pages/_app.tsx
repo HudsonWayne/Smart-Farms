a8c0140f43bf0c0189536bf73f6ed20bd46427ff
HudsonWayne <waynewaynebenhura@gmail.com>
2025-07-19 20:07:46 +0200

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "@/components/Navbar";
+import Carousel from "@/components/Carousel";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Navbar />
-      <main className="pt-16">
-        {/* Push content down so it doesn't hide behind the fixed navbar */}
+      <Carousel />
+      <main className="pt-8">
         <Component {...pageProps} />
       </main>
     </>
