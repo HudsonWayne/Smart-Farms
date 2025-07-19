f6d0027fe9f0b27f079de904dcabdf5b20a55001
HudsonWayne <waynewaynebenhura@gmail.com>
2025-07-19 23:15:40 +0200

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Footer from "@/components/Footer";
-
 import Navbar from "@/components/Navbar";
 import Carousel from "@/components/Carousel";
 import AboutSection from "@/components/AboutSection";
+import OurMission from "@/components/OurMission"; // ✅ Add this
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,16 +12,17 @@ export default function App({ Component, pageProps }: AppProps) {
       <Navbar />
       <Carousel />
 
-      {/* Add margin above AboutSection if needed */}
       <div className="mt-[20px]">
         <AboutSection />
       </div>
+
+      {/* ✅ Add OurMission section below AboutSection */}
+      <OurMission />
 
       <main className="pt-8">
         <Component {...pageProps} />
       </main>
 
-      {/* 👇 Footer at the bottom */}
       <Footer />
     </>
   );
